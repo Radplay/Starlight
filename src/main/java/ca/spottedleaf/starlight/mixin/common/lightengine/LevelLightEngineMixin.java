c966c68740f0527717096a89f4590291db949c6f
Radosław Ryszka <radoslaw.ryszka006@gmail.com>
2022-11-22 19:54:31 +0100

    Backport to 1.14. Original backport by KingContaria: https://github.com/dariasc/Starlight/commit/46cf6e3650dfe7fe790d34e3715946b6e2a31538

--- src/main/java/ca/spottedleaf/starlight/mixin/common/lightengine/LevelLightEngineMixin.java
+++ src/main/java/ca/spottedleaf/starlight/mixin/common/lightengine/LevelLightEngineMixin.java
@@ -158,6 +158,7 @@ public abstract class LevelLightEngineMixin implements LightEventListener, StarL
      * @reason Need to use our own hooks for retrieving light data
      * @author Spottedleaf
      */
+    /*
     @Overwrite
     public int getRawBrightness(final BlockPos pos, final int ambientDarkness) {
         // need to use new light hooks for this
@@ -165,6 +166,8 @@ public abstract class LevelLightEngineMixin implements LightEventListener, StarL
         final int block = this.lightEngine.getBlockReader().getLightValue(pos);
         return Math.max(sky, block);
     }
+
+     */
 
     @Unique
     protected final Long2ObjectOpenHashMap<SWMRNibbleArray[]> blockLightMap = new Long2ObjectOpenHashMap<>();
